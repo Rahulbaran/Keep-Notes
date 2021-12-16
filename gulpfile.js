ab780b1be0c737bf52185c3f3e56d52295b26ae9
Rahulbaran <rahulkumar109422@gmail.com>
2021-12-16 21:28:24 +0530

    Starter files created
    Required setup to start the projects has been created

---
+++ gulpfile.js
@@ -0,0 +1,53 @@
+const { src, dest, series, watch } = require("gulp");
+const scss = require("gulp-sass")(require("sass"));
+const postcss = require("gulp-postcss");
+const cssnano = require("cssnano");
+const prefixer = require("autoprefixer");
+const terser = require("gulp-terser");
+const srcMaps = require("gulp-sourcemaps");
+const browserSync = require("browser-sync").create();
+
+//scss Task
+const scssTask = function () {
+    return src("dist/scss/*.scss")
+        .pipe(scss())
+        .pipe(srcMaps.init())
+        .pipe(postcss([cssnano(), prefixer]))
+        .pipe(srcMaps.write("."))
+        .pipe(dest("./dist/css"));
+};
+
+//js Task
+const jsTask = function () {
+    return src("dist/js/*.js")
+        .pipe(terser())
+        .pipe(srcMaps.init())
+        .pipe(srcMaps.write("."))
+        .pipe(dest("./dist/minjs"));
+};
+
+//browser serve task
+const serveBrowser = function (start) {
+    browserSync.init({
+        server: "./dist/",
+        port: 5050,
+        logLevel: "debug",
+        browser: "brave",
+    });
+    start();
+};
+
+//reload Browser Task
+const reloadBrowser = function (start) {
+    browserSync.reload();
+    start();
+};
+
+//watch Task
+const watchTask = function () {
+    watch("./dist/*.html", reloadBrowser);
+    watch("./dist/scss/*.scss", series(scssTask, reloadBrowser));
+    watch("./dist/js/*.js", series(jsTask, reloadBrowser));
+};
+
+exports.default = series(scssTask, jsTask, serveBrowser, watchTask);
